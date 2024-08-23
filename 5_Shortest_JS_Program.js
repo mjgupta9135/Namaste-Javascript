3080d65bf382377631b4621c70e6930f1a6d55f9
Mrityunjay <mjguptacse@gmail.com>
2024-08-23 15:23:25 +0530

    Shortest JS program Done

---
+++ 5_Shortest_JS_Program.js
@@ -0,0 +1,31 @@
+/*
+Shortest Javascript program is an empty file.
+-> In Case of empty file JS create Global Execution Context and pushed into stack
+-> JS Engine also created something known as WINDOW.
+-> Window is like a big object with lots of function  and properties.
+-> These Functions and variables are created by  JS Engine into global space
+-> The JS Engine also creates this. At global level this points to window object.
+
+What is Global Space?
+Ans:- Any code which is not inside any function is global space.Any variable we write in the global space gets attached to the global object window.
+*/
+
+var a = 1;
+function c() {
+  var b = 3;
+}
+
+/*
+If we see in window  we will able to find variable a and function c but no able to find variable b because b is in functional scope 
+and variable a and function c is in global space. 
+In global space window ===this
+*/
+console.log(a);
+console.log(window.a);
+console.log(this.a);
+/*
+All three will give same answer
+if we dont write anything before a then it will assume that a is in window object.
+*/
+console.log(b);
+// In above case JS engine search for b in window object but b is not in window object so it will give refrence error b is not defines
