51f8b703bb87b28974baf276dcb5bdfeb40b9ffe
Mrityunjay <mjguptacse@gmail.com>
2024-08-23 11:56:50 +0530

    First Lecture Finished

--- 1_How-Javascript-works.js
+++ 1_How-Javascript-works.js
@@ -1,1 +1,14 @@
-console.log("Hi");
+/*
+Javascript is a synchronous single-threaded language
+Synchronous ---------> Commands are executed in a specific order
+Single Threaded -----> One command at a time
+That means Javascript can run one command at a time in a specific order 
+It can only go to next line if current line has been finished executing.
+
+
+Everything happens in Javascript happens inside Execution Context
+We can assume this execution context as a big box or whole container where JavaScript Code is executed.
+Execution context has two components 
+1.Memory Component(Variable Environment) ---->In Memory component the variable and functions are stored as key value pairs
+2.Code Component(Thread of Execution) ----> In Code Component the whole code is executed one line at a time
+*/
